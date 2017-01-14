1b4713505494922f7b0a0f88c0b3f88bf55a19e0
Magnus Edenhill <magnus@edenhill.se>
2017-01-14 11:45:01 +0100

    Rework Win32 builds

--- win32/build.bat
+++ win32/build.bat
@@ -1,8 +1,23 @@
-msbuild librdkafka.sln /p:Configuration="Debug" /p:Platform="Win32"
+@echo off
 
-msbuild librdkafka.sln /p:Configuration="Release" /p:Platform="Win32"
+SET TOOLCHAIN=v140
 
-msbuild librdkafka.sln /p:Configuration="Debug" /p:Platform="x64"
+FOR %%C IN (Debug,Release) DO (
+  FOR %%P IN (Win32,x64) DO (
+     @echo Building %%C %%P
+     msbuild librdkafka.sln /p:Configuration=%%C /p:Platform=%%P /target:Clean
+     msbuild librdkafka.sln /p:Configuration=%%C /p:Platform=%%P || goto :error
 
-msbuild librdkafka.sln /p:Configuration="Release" /p:Platform="x64"
+     REM lib /OUT:outdir\%TOOLCHAIN%\%%P\%%C\librdkafka-static.lib interim\%TOOLCHAIN%/%%P\%%C\*.obj
+     REM @echo "Static C library in outdir\%TOOLCHAIN%\%%P\%%C\librdkafka-static.lib"
 
+     REM lib /OUT:outdir\%TOOLCHAIN%\%%P\%%C\librdkafkacpp-static.lib librdkafkacpp\interim\%TOOLCHAIN%/%%P\%%C\*.obj
+     REM @echo "Static C++ library in outdir\%TOOLCHAIN%\%%P\%%C\librdkafkacpp-static.lib"
+  )
+)
+
+exit /b 0
+
+:error
+echo "Build failed"
+exit /b 1
--- win32/package-nuget.ps1
+++ win32/package-nuget.ps1
@@ -16,6 +16,18 @@
    Requires CoApp
 #>
 
+param ([string]$build_version = "0.9.3-pre1")
 
+# Make the tools available for later PS scripts to use.
+$env:PSModulePath = $env:PSModulePath + ';C:\Program Files (x86)\Outercurve Foundation\Modules'
+Import-Module CoApp
 
-Write-NuGetPackage librdkafka.autopkg
+# This is the CoApp .autopkg file to create.
+$autopkgFile = "librdkafka.autopkg"
+
+# Get the ".autopkg.template" file, replace "@version" with the Appveyor version number, then save to the ".autopkg" file.
+cat ($autopkgFile + ".template") | % { $_ -replace "@version", $build_version } > $autopkgFile
+
+# Use the CoApp tools to create NuGet native packages from the .autopkg.
+Write-NuGetPackage $autopkgFile
+
