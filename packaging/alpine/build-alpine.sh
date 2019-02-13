677f7820818da079ac83fa2dfe8d502075e17060
Magnus Edenhill <magnus@edenhill.se>
2019-02-13 09:06:57 +0100

    mklove: add --install-deps to automatically install missing dependencies, and more
    and --source-deps-only to force installation from source rather than
    to rely on package manager (which may have outdated versions).
    
    This moves the myriad of bootstrap-librdkafka.sh functionality to
    librdkafka's build tree, where it belongs.
    
    Renamed:
     --enable-sasl -> --enable-gssapi
     --enable-lz4 -> --enable-lz4-ext
    
    The old names remain as aliases for the time being.
    
    Now also honours --disable-gssapi and --disable-ssl.

---
+++ packaging/alpine/build-alpine.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Build librdkafka on Alpine.
+#
+
+set -x
+
+if [ "$1" = "--in-docker" ]; then
+    # Runs in docker, performs the actual build.
+
+    apk add bash curl gcc g++ make musl-dev bsd-compat-headers git python perl
+
+    git clone /v /librdkafka
+
+    cd /librdkafka
+    ./configure --install-deps --disable-gssapi --disable-lz4-ext --enable-static
+    make -j
+    make -C tests run_local
+
+    # Create a tarball in artifacts/
+    cd src
+    tar cvzf /v/artifacts/alpine-librdkafka.tgz librdkafka.so.1
+    cd ../..
+
+else
+    # Runs on the host, simply spins up the in-docker build.
+    if [ ! -f configure.self ]; then
+        echo "Must be run from the top-level librdkafka dir"
+        exit 1
+    fi
+
+    exec docker run -v $PWD:/v alpine:3.8 /v/packaging/alpine/$(basename $0) --in-docker
+fi
